08e757bb7f1456669353a08b0997fb499f0b8174
Miriam Kreher <mkreher2@hs-mittweida.de>
2020-12-10 15:10:18 +0100

    data seperation for training and testing

--- dataaug/basicaug.py
+++ dataaug/basicaug.py
@@ -5,18 +5,25 @@ import nlpaug.augmenter.word as naw
 import nlpaug.augmenter.sentence as nas
 
 from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
 
 
 #IMPORT DATA
 movie_data = load_files(r".\dataset\txt_sentoken")
 #target y, neg and pos in X
 X, y = movie_data.data, movie_data.target
-print(len(X))
+
+#divide data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+print("training data size: ", len(X_train))
+print("test data size: ", len(X_test))
+
 for sen in range(0,1) :
     #data aug on first review
     aug = nac.KeyboardAug()
-    augmented_text = aug.augment(X[sen].decode('utf-8'))
+    augmented_text = aug.augment(X_train[sen].decode('utf-8'))
     print("Original:")
-    print(X[sen])
+    print(X_train[sen].decode('utf-8'))
     print("Augmented Text:")
     print(augmented_text)
---
+++ dataset/data_seperator.py
@@ -0,0 +1,40 @@
+import os, random
+import shutil
+
+def  copytraindata(type):
+    if(type == 'n'):
+        seperate = 'neg/'
+    if(type == 'p'):
+        seperate = 'pos/'
+    path = 'nlp_dataaug/dataset/'
+    data_org = os.listdir('nlp_dataaug/dataset/txt_sentoken/'+seperate)
+
+    #calc proportion/ amount for test and training set
+    trainingsize=int(len(data_org)*0.8)
+    orgpath = path+'/txt_sentoken/'+seperate
+    destpath_train = 'nlp_dataaug/dataset/trainingset/'+seperate
+    destpath_test=  'nlp_dataaug/dataset/testset/'+seperate
+
+    #select files in range of trainingsize,  copy& move them to new folder
+    print(trainingsize)
+    rfs= []
+    ofs = []
+    for d in data_org:
+        ofs.append(d)
+
+    for d in range(0, trainingsize):
+        rf= random.choice(data_org)
+        while rf in rfs:
+            #randomly selected file is aleady selected
+            rf= random.choice(data_org)
+        rfs.append(rf)
+        shutil.copy2(orgpath+str(rf),destpath_train)
+
+    #find the ones that are in org. but not trainingset
+    for t in ofs:
+        if t not in rfs: 
+            print('not'+t)
+            shutil.copy2(orgpath+t, destpath_test)
+
+copytraindata('p')
+copytraindata('n')
